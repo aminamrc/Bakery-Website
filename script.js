b70ef47c3b643441167b40f202c9f096b002c120
aminamrc <amina.mrc@gmail.com>
2024-04-02 11:32:18 +0200

    added queries and finished

--- script.js
+++ script.js
@@ -2,14 +2,32 @@ const searchIcon = document.querySelector(".search-icon");
 const searchForm = document.querySelector (".search-form");
 
 searchIcon.addEventListener ('click', () => {
-    searchForm.classList.add("active");
+    searchForm.classList.toggle("active");
     cartItemsContainer.classList.remove("active");
+    navBar.classList.remove("active");
 });
 
 const cartIcon = document.querySelector(".cart-icon");
 const cartItemsContainer = document.querySelector (".cart-items-container");
 
 cartIcon.addEventListener ('click', () => {
-    cartItemsContainer.classList.add("active");
+    cartItemsContainer.classList.toggle("active");
+    searchForm.classList.remove("active");
+    navBar.classList.remove("active");
+});
+
+
+const menuIcon = document.querySelector (".menu-icon");
+const navBar = document.querySelector (".navbar");
+
+menuIcon.addEventListener ('click', () => {
+    navBar.classList.toggle("active");
+    cartItemsContainer.classList.remove("active");
     searchForm.classList.remove("active");
 });
+
+window.onscroll= ()=> {
+    navBar.classList.remove("active");
+    cartItemsContainer.classList.remove("active");
+    searchForm.classList.remove("active");
+}
